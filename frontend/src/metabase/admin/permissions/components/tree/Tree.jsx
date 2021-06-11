ded2e1ccc94cd9f154d44de559a17040ef686fa1
alxnddr <alxnddr@gmail.com>
2021-06-11 20:45:40 +0300

    add UI components for the new permissions page

--- frontend/src/metabase/admin/permissions/components/tree/Tree.jsx
+++ frontend/src/metabase/admin/permissions/components/tree/Tree.jsx
@@ -7,11 +7,18 @@ const propTypes = {
   TreeNodeComponent: PropTypes.object,
   data: PropTypes.array.isRequired,
   onSelect: PropTypes.func.isRequired,
+  selectedId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  emptyState: PropTypes.node,
 };
 
-export function Tree({ TreeNodeComponent = TreeNode, data, onSelect }) {
+export function Tree({
+  TreeNodeComponent = TreeNode,
+  data,
+  onSelect,
+  selectedId,
+  emptyState = null,
+}) {
   const [expandedIds, setExpandedIds] = useState(new Set());
-  const [selectedId, setSelectedId] = useState(null);
 
   const handleToggleExpand = useCallback(
     itemId => {
@@ -26,24 +33,25 @@ export function Tree({ TreeNodeComponent = TreeNode, data, onSelect }) {
 
   const handleSelect = useCallback(
     itemId => {
-      setSelectedId(itemId);
       onSelect(itemId);
     },
     [onSelect],
   );
 
+  if (data.length === 0) {
+    return <React.Fragment>{emptyState}</React.Fragment>;
+  }
+
   return (
-    <div>
-      <TreeNodeList
-        TreeNodeComponent={TreeNodeComponent}
-        items={data}
-        onSelect={handleSelect}
-        onToggleExpand={handleToggleExpand}
-        expandedIds={expandedIds}
-        selectedId={selectedId}
-        depth={0}
-      />
-    </div>
+    <TreeNodeList
+      TreeNodeComponent={TreeNodeComponent}
+      items={data}
+      onSelect={handleSelect}
+      onToggleExpand={handleToggleExpand}
+      expandedIds={expandedIds}
+      selectedId={selectedId}
+      depth={0}
+    />
   );
 }
 
