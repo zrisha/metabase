7fc3d24a440a1e1160de97dd312c11c4179cfa28
Zak Risha <zjrisha@gmail.com>
2024-01-01 12:59:34 -0500

    add session ids to logging

--- src/metabase/api/user_activity.clj
+++ src/metabase/api/user_activity.clj
@@ -8,10 +8,11 @@
             [toucan.db :as db]))
 
 (api/defendpoint PUT "/"
-  [:as {{:keys [activity]} :body}]
+  [:as {:keys [body, cookies]}]
   (api.user/check-self-or-superuser api/*current-user-id*)
   (db/insert! UserActivity
-          :activity activity
+          :activity (:activity body)
+          :session_id (get-in cookies ["metabase.SESSION" :value])
           :user_id api/*current-user-id*))
 
   (api/define-routes)
